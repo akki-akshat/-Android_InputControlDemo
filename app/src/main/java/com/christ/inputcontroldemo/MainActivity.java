9d7cb6403929f43007296e5748f5604c5d17ee79
Akshat Ahuja <akshatahuja03@gmail.com>
2018-08-22 01:49:46 +0530

    Checkbox and fragments working

--- app/src/main/java/com/christ/inputcontroldemo/MainActivity.java
+++ app/src/main/java/com/christ/inputcontroldemo/MainActivity.java
@@ -2,7 +2,6 @@ package com.christ.inputcontroldemo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -17,19 +16,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        btn=(Button)findViewById(R.id.button2);
-        d=(DatePicker)findViewById(R.id.datePicker);
-        new CountDownTimer(3000,1000){
-            @Override
-            public void onTick(long undef){}
-                public void onFinish(){
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button2);
+        d = (DatePicker) findViewById(R.id.datePicker);
+//        new CountDownTimer(3000, 1000) {
+//            @Override
+//            public void onTick(long undef) {
+//            }
+//
+//            public void onFinish() {
+//
+//                MainActivity.this.setContentView(R.layout.activity_main);
+//            }
+//        }.start();
 
-                MainActivity.this.setContentView(R.layout.activity_main);
-                }
-        }.start();
         btn.setOnClickListener(new View.OnClickListener() {
-            @Override
+              @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this,TimeActivity.class);
                 startActivity(i);
--- app/src/main/java/com/christ/inputcontroldemo/Menu.java
+++ app/src/main/java/com/christ/inputcontroldemo/Menu.java
@@ -1,13 +1,43 @@
 package com.christ.inputcontroldemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Menu extends AppCompatActivity {
 
+    RadioGroup rg;
+    RadioButton veg;
+    RadioButton nveg;
+    FNveg nv = new FNveg();
+    Fveg ve = new Fveg();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        rg=(RadioGroup)findViewById(R.id.radioGroup);
+        veg=(RadioButton)findViewById(R.id.vegB);
+        nveg=(RadioButton)findViewById(R.id.nonvegB);
+        final Fveg ve = new Fveg();
+        final FNveg nv = new FNveg();
+        veg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment,ve);
+                fragmentTransaction.commit();
+            }
+        });
+        nveg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment,nv);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
